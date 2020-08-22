4f2bb3623cc1ccdc729ae9f1175f9e13dc1f167f
popai2107 <69206759+popai2107@users.noreply.github.com>
2020-08-22 16:13:49 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,115 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <h1>Anime Photo!</h1>
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class></li>
+          <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="2" class></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item">
+            <img :src="img[0].src" class="img-fluid" />
+          </div>
+          <div class="carousel-item active">
+            <img :src="img[1].src" class="img-fluid" />
+          </div>
+          <div class="carousel-item">
+            <img :src="img[2].src" class="img-fluid" />
+          </div>
+          <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <hr class="featurette-divider" />
+      <div class="row featurette alert alert-success" style="opacity: 0.9; ">
+        <div class="row align-items-center">
+          <div class="col-6">
+            <!-- 2.รับค่าจาก emit @cc แล้วส่งไปทำงานใน function "sum"-->
+            <like label @cc="sum" />
+          </div>
+          <div class="col-6">
+            <img :src="img[0].src" class="img-fluid" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <hr class="featurette-divider" />
+      <div class="row featurette alert alert-success" style="opacity: 0.9; ">
+        <div class="row align-items-center">
+          <div class="col-6">
+            <img :src="img[1].src" class="img-fluid" />
+          </div>
+          <div class="col-6">
+            <!-- 2.รับค่าจาก emit @cc แล้วส่งไปทำงานใน function "sum"-->
+            <like label @cc="sum" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <hr class="featurette-divider" />
+      <div class="row featurette alert alert-success" style="opacity: 0.9; ">
+        <div class="row align-items-center">
+          <div class="col-6">
+            <!-- 2.รับค่าจาก emit @cc แล้วส่งไปทำงานใน function "sum"-->
+            <like label @cc="sum" />
+          </div>
+          <div class="col-6">
+            <img :src="img[2].src" class="img-fluid" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Like from "./components/Like.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Like },
+  data: function () {
+    return {
+      img: [
+        {
+          src:
+            "https://i.pinimg.com/564x/a4/31/fb/a431fb6aee6aab472ac7ea254ab5d538.jpg",
+        },
+        {
+          src:
+            "https://i.pinimg.com/564x/76/3d/79/763d7979696b50c81cacfed3f8e72d60.jpg",
+        },
+        {
+          src:
+            "https://i.pinimg.com/564x/05/9c/bd/059cbd7c6a229ed47b3f38e2c3052888.jpg",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Do Hyeon", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ffffff;
   margin-top: 60px;
+}
+body {
+  background-image: url("./assets/BG02.png");
 }
 </style>
